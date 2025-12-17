7f8fa35226610f4434db1d15c140db40b0a1fdd2
YogirajSharma <yogirajsharma482@gmail.com>
2025-12-17 22:41:25 +0530

    Added changes in main.py and ip_verification.py file

--- IP_verification.py
+++ IP_verification.py
@@ -1,38 +1,38 @@
-import ipaddress
-import re
+import ipaddress # Built-in module to validate IP addresses and networks.
+import re # Used for regex pattern matching.
 
-def detect_target_type(target: str):
-    """
-    Detect whether the provided target is:
-    - CIDR (e.g., 192.168.1.0/24)
-    - Single IP (IPv4 or IPv6)
-    - IP range (e.g., 192.168.1.10-20)
-    - Domain (example.com)
-    """
-
+def detect_target_type(target: str) -> str:
     target = target.strip()
+
+    # CIDR Detection
     try:
         if "/" in target:
-            ipaddress.ip_network(target, strict=False)
+            ipaddress.ip_network(target, strict=False) # Validates IPv4 or IPv6 address. strict=False allows non-network addresses.
             return "CIDR"
     except ValueError:
         pass
+
+    # Single IP Detection
     try:
         ipaddress.ip_address(target)
         return "IP"
     except ValueError:
         pass
+
+    # IP Range Detection(192.168.1.10-20)
     range_pattern = r"^(\d{1,3}\.){3}\d{1,3}-\d{1,3}$"
     if re.match(range_pattern, target):
-        base_ip, range_end = target.split("-")
+        base_ip, last_octet = target.split("-")
         try:
             ipaddress.ip_address(base_ip)
-            last_octet = int(range_end)
-            if 0 <= last_octet <= 255:
+            if 0 <= int(last_octet) <= 255:
                 return "range"
         except ValueError:
             pass
+
+    # Domain Detection
     domain_pattern = r"^(?!-)([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$"
     if re.match(domain_pattern, target):
         return "domain"
-    return "Invalid target format"
+
+    return "invalid"
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from IP_verification import *
-target = input("Enter IP address")
-retrun_type= detect_target_type(target)
-print(retrun_type)
+import json # Imports the built-in json module to format output as JSON.
+from Scanner import scan_target # Imports the scan_target function from (Scanner.py).
+
+def main():
+    target = input("Enter IP / CIDR / range / domain: ").strip()
+    output = scan_target(target)
+
+    if output:
+        print(json.dumps(output, indent=4)) # Converts Python dictionary → formatted JSON output.
+    else:
+        print("[-] No scan results returned.") # Handles empty or failed scans.
+# Ensures the script only runs when executed directly (not imported)
+if __name__ == "__main__":
+    main()
